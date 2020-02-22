dbf4d48d64e6a582beb8c11661952c749610951f
Fisher <32707008+FisherWY@users.noreply.github.com>
2020-02-22 22:36:03 +0800

    226，231，232完成

---
+++ src/solution_ez/solution231.java
@@ -0,0 +1,47 @@
+package solution_ez;
+
+/**
+ * @Author Fisher
+ * @Date 2020/2/22 21:59
+ **/
+
+/**
+ * 给定一个整数，编写一个函数来判断它是否是 2 的幂次方。
+ *
+ * 示例 1:
+ *
+ * 输入: 1
+ * 输出: true
+ * 解释: 20 = 1
+ * 示例 2:
+ *
+ * 输入: 16
+ * 输出: true
+ * 解释: 24 = 16
+ * 示例 3:
+ *
+ * 输入: 218
+ * 输出: false
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/power-of-two
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class solution231 {
+
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        return (n & (n - 1)) == 0;
+    }
+
+    public static void main(String[] args) {
+        solution231 s = new solution231();
+        if (s.isPowerOfTwo(4)) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
+}
---
+++ src/solution_ez/solution232.java
@@ -0,0 +1,92 @@
+package solution_ez;
+
+/**
+ * @Author Fisher
+ * @Date 2020/2/22 22:13
+ **/
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 使用栈实现队列的下列操作：
+ *
+ * push(x) -- 将一个元素放入队列的尾部。
+ * pop() -- 从队列首部移除元素。
+ * peek() -- 返回队列首部的元素。
+ * empty() -- 返回队列是否为空。
+ * 示例:
+ *
+ * MyQueue queue = new MyQueue();
+ *
+ * queue.push(1);
+ * queue.push(2);
+ * queue.peek();  // 返回 1
+ * queue.pop();   // 返回 1
+ * queue.empty(); // 返回 false
+ * 说明:
+ *
+ * 你只能使用标准的栈操作 -- 也就是只有 push to top, peek/pop from top, size, 和 is empty 操作是合法的。
+ * 你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
+ * 假设所有操作都是有效的 （例如，一个空的队列不会调用 pop 或者 peek 操作）。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/implement-queue-using-stacks
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class solution232 {
+
+    static class MyQueue {
+
+        Stack<Integer> stack;
+        Stack<Integer> tmp;
+
+        /** Initialize your data structure here. */
+        public MyQueue() {
+            stack = new Stack<>();
+            tmp = new Stack<>();
+        }
+
+        /** Push element x to the back of queue. */
+        public void push(int x) {
+            while (!stack.isEmpty()) {
+                tmp.push(stack.pop());
+            }
+            stack.push(x);
+            while (!tmp.isEmpty()) {
+                stack.push(tmp.pop());
+            }
+        }
+
+        /** Removes the element from in front of queue and returns that element. */
+        public int pop() {
+            return stack.pop();
+        }
+
+        /** Get the front element. */
+        public int peek() {
+            return stack.peek();
+        }
+
+        /** Returns whether the queue is empty. */
+        public boolean empty() {
+            return stack.isEmpty();
+        }
+    }
+
+    public static void main(String[] args) {
+        MyQueue myQueue = new MyQueue();
+        myQueue.push(1);
+        myQueue.push(2);
+        System.out.println(myQueue.peek());
+        System.out.println(myQueue.pop());
+        if (myQueue.empty()) {
+            System.out.println("Empty");
+        } else {
+            System.out.println("Not Empty");
+        }
+    }
+
+
+}
