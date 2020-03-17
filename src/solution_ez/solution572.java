79191ee49dd67159dcd359061e2a2a34b57ca18d
Fisher <32707008+FisherWY@users.noreply.github.com>
2020-03-17 22:23:35 +0800

    566，572，575完成

---
+++ src/solution_ez/solution572.java
@@ -0,0 +1,109 @@
+package solution_ez;
+
+/**
+ * @Author Fisher
+ * @Date 2020/3/17 21:41
+ **/
+
+import java.util.Stack;
+
+/**
+ * 给定两个非空二叉树 s 和 t，检验 s 中是否包含和 t 具有相同结构和节点值的子树。
+ * s 的一个子树包括 s 的一个节点和这个节点的所有子孙。s 也可以看做它自身的一棵子树。
+ *
+ * 示例 1:
+ * 给定的树 s:
+ *
+ *      3
+ *     / \
+ *    4   5
+ *   / \
+ *  1   2
+ * 给定的树 t：
+ *
+ *    4
+ *   / \
+ *  1   2
+ * 返回 true，因为 t 与 s 的一个子树拥有相同的结构和节点值。
+ *
+ * 示例 2:
+ * 给定的树 s：
+ *
+ *      3
+ *     / \
+ *    4   5
+ *   / \
+ *  1   2
+ *     /
+ *    0
+ * 给定的树 t：
+ *
+ *    4
+ *   / \
+ *  1   2
+ * 返回 false。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/subtree-of-another-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class solution572 {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null || t == null) {
+            return false;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(s);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (fun(node, t)) {
+                return true;
+            }
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return false;
+    }
+
+    private boolean fun(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) {
+            return true;
+        } else if (node1 == null || node2 == null) {
+            return false;
+        }
+        return (node1.val == node2.val) && (fun(node1.left, node2.left)) && (fun(node1.right, node2.right));
+    }
+
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(3);
+        TreeNode node2 = new TreeNode(4);
+        TreeNode node3 = new TreeNode(5);
+        TreeNode node4 = new TreeNode(1);
+        TreeNode node5 = new TreeNode(2);
+        TreeNode node6 = new TreeNode(0);
+        node1.left = node2;
+        node1.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+        node5.left = node6;
+
+        solution572 s = new solution572();
+        if (s.isSubtree(node1, node2)) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
+}
