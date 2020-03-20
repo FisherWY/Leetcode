d479515577fa1fcac57f8316189de37c0e937c01
Fisher <32707008+FisherWY@users.noreply.github.com>
2020-03-20 22:18:26 +0800

    605，606，617完成

---
+++ src/solution_ez/solution605.java
@@ -0,0 +1,61 @@
+package solution_ez;
+
+/**
+ * @Author Fisher
+ * @Date 2020/3/20 21:25
+ **/
+
+/**
+ * 假设你有一个很长的花坛，一部分地块种植了花，另一部分却没有。可是，花卉不能种植在相邻的地块上，它们会争夺水源，两者都会死去。
+ * 给定一个花坛（表示为一个数组包含0和1，其中0表示没种植花，1表示种植了花），和一个数 n 。
+ * 能否在不打破种植规则的情况下种入 n 朵花？能则返回True，不能则返回False。
+ *
+ * 示例 1:
+ * 输入: flowerbed = [1,0,0,0,1], n = 1
+ * 输出: True
+ *
+ * 示例 2:
+ * 输入: flowerbed = [1,0,0,0,1], n = 2
+ * 输出: False
+ *
+ * 注意:
+ * 数组内已种好的花不会违反种植规则。
+ * 输入的数组长度范围为 [1, 20000]。
+ * n 是非负整数，且不会超过输入数组的大小。
+ */
+public class solution605 {
+
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (i - 1 >= 0) {
+                if (flowerbed[i - 1] == 1) {
+                    continue;
+                }
+            }
+            if (i + 1 < flowerbed.length) {
+                if (flowerbed[i + 1] == 1) {
+                    i += 2;
+                    continue;
+                }
+            }
+            if (flowerbed[i] == 0) {
+                count++;
+                flowerbed[i]++;
+                i++;
+            }
+        }
+        return count >= n;
+    }
+
+    public static void main(String[] args) {
+        int[] flowerbed = {1,0,0,0,1};
+        int n = 1;
+        solution605 s = new solution605();
+        if (s.canPlaceFlowers(flowerbed, n)) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
+}
