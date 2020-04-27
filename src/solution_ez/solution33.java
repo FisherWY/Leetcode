0d775a80db0f58f50dba8bffc9add5724376cdf5
Fisher <32707008+FisherWY@users.noreply.github.com>
2020-04-27 23:47:06 +0800

    33、1051、1071、1078完成

---
+++ src/solution_ez/solution33.java
@@ -0,0 +1,66 @@
+package solution_ez;
+
+/**
+ * @Author Fisher
+ * @Date 2020/4/27 22:50
+ **/
+
+import com.sun.org.apache.bcel.internal.generic.IF_ACMPEQ;
+
+/**
+ * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+ * ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+ * 搜索一个给定的目标值，如果数组中存在这个目标值，则返回它的索引，否则返回 -1 。
+ * 你可以假设数组中不存在重复的元素。
+ * 你的算法时间复杂度必须是 O(log n) 级别。
+ *
+ * 示例 1:
+ * 输入: nums = [4,5,6,7,0,1,2], target = 0
+ * 输出: 4
+ *
+ * 示例 2:
+ * 输入: nums = [4,5,6,7,0,1,2], target = 3
+ * 输出: -1
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/search-in-rotated-sorted-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class solution33 {
+
+    public int search(int[] nums, int target) {
+        if (nums.length == 0) {
+            return -1;
+        }
+        int start = 0, end = nums.length - 1, mid;
+        while (start <= end) {
+            mid = (start + end) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            // 前半部分有序
+            if (nums[start] <= nums[mid]) {
+                // target在前半部分
+                if (target >= nums[start] && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target <= nums[end] && target > nums[mid]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4,5,6,7,0,1,2};
+        int target = 3;
+        solution33 s = new solution33();
+        System.out.println(s.search(nums, target));
+    }
+}
