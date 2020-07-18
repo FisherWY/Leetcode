154ba64b8b4c4ad67700206be5b8e3a460a7faf7
Fisher <32707008+FisherWY@users.noreply.github.com>
2020-07-18 18:23:35 +0800

    97、1314、1315完成

---
+++ src/solution_hard/solution97.java
@@ -0,0 +1,51 @@
+package solution_hard;
+
+/**
+ * @Author Fisher
+ * @Date 2020/7/18 16:08
+ **/
+
+/**
+ * 给定三个字符串 s1, s2, s3, 验证 s3 是否是由 s1 和 s2 交错组成的。
+ *
+ * 示例 1:
+ * 输入: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+ * 输出: true
+ *
+ * 示例 2:
+ * 输入: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
+ * 输出: false
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/interleaving-string
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class solution97 {
+
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int len1 = s1.length(), len2 = s2.length(), len3 = s3.length();
+        if (len1 + len2 != len3) {
+            return false;
+        }
+        boolean[][] dp = new boolean[len1 + 1][len2 + 1];
+        dp[0][0] = true;
+        for (int i = 0; i <= len1; i++) {
+            for (int j = 0; j <= len2; j++) {
+                int p = i + j - 1;
+                if (i > 0) {
+                    dp[i][j] = dp[i][j] || (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(p));
+                }
+                if (j > 0) {
+                    dp[i][j] = dp[i][j] || (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(p));
+                }
+            }
+        }
+        return dp[len1][len2];
+    }
+
+    public static void main(String[] args) {
+        String s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac";
+        solution97 s = new solution97();
+        System.out.println(s.isInterleave(s1, s2, s3));
+    }
+}
