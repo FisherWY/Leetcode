4cacb12b815d13ff7586db340f6203d1a7786717
Fisher <32707008+FisherWY@users.noreply.github.com>
2020-08-07 17:57:07 +0800

    336、1282完成

---
+++ src/solution_hard/solution336.java
@@ -0,0 +1,128 @@
+package solution_hard;
+
+/**
+ * @Author Fisher
+ * @Date 2020/8/6 19:41
+ **/
+
+import java.util.*;
+
+/**
+ * 给定一组 互不相同 的单词， 找出所有不同 的索引对(i, j)，使得列表中的两个单词， words[i] + words[j] ，可拼接成回文串。
+ *
+ * 示例 1：
+ * 输入：["abcd","dcba","lls","s","sssll"]
+ * 输出：[[0,1],[1,0],[3,2],[2,4]]
+ * 解释：可拼接成的回文串为 ["dcbaabcd","abcddcba","slls","llssssll"]
+ *
+ * 示例 2：
+ * 输入：["bat","tab","cat"]
+ * 输出：[[0,1],[1,0]]
+ * 解释：可拼接成的回文串为 ["battab","tabbat"]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/palindrome-pairs
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class solution336 {
+
+    List<String> wordsRev = new ArrayList<String>();
+    Map<String, Integer> indices = new HashMap<String, Integer>();
+
+    public List<List<Integer>> palindromePairs(String[] words) {
+        int n = words.length;
+        for (String word: words) {
+            wordsRev.add(new StringBuffer(word).reverse().toString());
+        }
+        for (int i = 0; i < n; ++i) {
+            indices.put(wordsRev.get(i), i);
+        }
+
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        for (int i = 0; i < n; i++) {
+            String word = words[i];
+            int m = words[i].length();
+            if (m == 0) {
+                continue;
+            }
+            for (int j = 0; j <= m; j++) {
+                if (isPalindrome(word, j, m - 1)) {
+                    int leftId = findWord(word, 0, j - 1);
+                    if (leftId != -1 && leftId != i) {
+                        ret.add(Arrays.asList(i, leftId));
+                    }
+                }
+                if (j != 0 && isPalindrome(word, 0, j - 1)) {
+                    int rightId = findWord(word, j, m - 1);
+                    if (rightId != -1 && rightId != i) {
+                        ret.add(Arrays.asList(rightId, i));
+                    }
+                }
+            }
+        }
+        return ret;
+    }
+
+    public boolean isPalindrome(String s, int left, int right) {
+        int len = right - left + 1;
+        for (int i = 0; i < len / 2; i++) {
+            if (s.charAt(left + i) != s.charAt(right - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int findWord(String s, int left, int right) {
+        return indices.getOrDefault(s.substring(left, right + 1), -1);
+    }
+
+    /**
+     * 超时答案
+     * @param words
+     * @return
+     */
+    public List<List<Integer>> palindromePairs1(String[] words) {
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < words.length - 1; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+                if (fun(words[i] + words[j])) {
+                    List<Integer> list = new ArrayList<>(2);
+                    list.add(i);
+                    list.add(j);
+                    res.add(list);
+                }
+                if (fun(words[j] + words[i])) {
+                    List<Integer> list = new ArrayList<>(2);
+                    list.add(j);
+                    list.add(i);
+                    res.add(list);
+                }
+            }
+        }
+        return res;
+    }
+
+    private boolean fun(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"abcd","dcba","lls","s","sssll"};
+        solution336 s = new solution336();
+        for (List<Integer> list : s.palindromePairs(words)) {
+            for (Integer x : list) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+    }
+}
