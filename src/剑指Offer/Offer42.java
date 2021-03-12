67258dec891afeb33012834ee54f6e2a86125324
Fisher <32707008+FisherWY@users.noreply.github.com>
2021-03-12 20:03:27 +0800

    剑指Offer42、43、44完成

---
+++ src/剑指Offer/Offer42.java
@@ -0,0 +1,15 @@
+package 剑指Offer;
+
+public class Offer42 {
+
+    public int maxSubArray(int[] nums) {
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] > 0) {
+                nums[i] += nums[i - 1];
+            }
+            res = Math.max(nums[i], res);
+        }
+        return res;
+    }
+}
